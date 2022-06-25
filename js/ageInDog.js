4002a5934c5962fdcb93cee0aba3123f7a7b68ac
alajido <96017433+Alajido@users.noreply.github.com>
2022-06-25 23:47:00 +0100

    age in dog added

---
+++ js/ageInDog.js
@@ -0,0 +1,19 @@
+/ variable that stores earlyAge
+var earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// subtracting 2 from myAge variable
+var laterYears = myAge - 2;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+// variabl that stores my age in dog years
+var myAgeInDogYears = earlyYears + laterYears;
+
+myName = "abubakar".toLowerCase();
+
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years.`
+);
