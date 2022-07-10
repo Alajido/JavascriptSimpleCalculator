054a9df87a4717e0dc10c051a9e583ff5b52cdfb
alajido <96017433+Alajido@users.noreply.github.com>
2022-07-10 07:32:50 +0100

    converter

--- js/ageInDays.js
+++ js/ageInDays.js
@@ -1,28 +1,31 @@
-let click = document.getElementById('click');
+let clickMe = document.getElementById('clickMe');
 let reset = document.getElementById('clear');
-let display = document.getElementById('display');
-let text = document.getElementById('text');
 
-click.addEventListener('click', () => {
-    var userAge = prompt('How old are you?');
-    var result = (2022 - userAge) * 360;
-    // text.innerHTML = 'you are ' + result + ' in days';
-    console.log(result)
-    var h1 = document.createElement('h1');
-    var note = document.createTextNode('you are ' + result + ' in days');
-    h1.appendChild(note);
-    document.getElementById('display').appendChild(h1);  
+clickMe.addEventListener('click', () => {
+    let userInput = prompt('Enter your birth year! ')
+    let result = (2022 - userInput) * 360;
+    let h1 = document.createElement('h1');
+    let text = document.createTextNode('you are ' + result + ' years in days!');
+    h1.appendChild(text);
+    h1.setAttribute('id', 'message');
+    let display = document.getElementById('display');
+    display.appendChild(h1)
+
 })
-
-
 
 reset.addEventListener('click', () => {
-    display.remove()
-    box.remove()
+    message.remove()
 })
 
+
+
 // function imageGenerator() {
-//     var image = document.createElement('img');
-//     image.src = 'explainer-how-bad-is-the-hack-that-targeted-us-agencies.jpg';
-//     document.getElementById('box').appendChild(image);
+//      var image = document.createElement('img');
+//      image.src = 'explainer-how-bad-is-the-hack-that-targeted-us-agencies.jpg';
+//      document.getElementById('box').appendChild(image);
 // }
+// generate.addEventListener('click', () => {
+//     var image = document.createElement('img').style.width = '100px';
+//     image.src = 'explainer-how-bad-is-the-hack-that-targeted-us-agencies.jpg'
+//     document.getElementById('box').appendChild(image);
+// })
